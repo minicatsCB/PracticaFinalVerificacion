b2392a9f0988790f800bb5ee5b600c1b6f16d247
minicatsCB <maribeldiaz96@yahoo.es>
2017-05-25 00:26:00 +0200

    Configure MongoDB

--- words/words/settings.py
+++ words/words/settings.py
@@ -64,9 +64,13 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'words.pipelines.WordsPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'words.pipelines.WordsPipeline': 300,
+}
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "words"
+MONGODB_COLLECTION = "paragraphs"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
