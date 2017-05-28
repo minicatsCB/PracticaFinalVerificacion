54a44bd7b9b3bba7263b378e3e865afee4ad579d
minicatsCB <maribeldiaz96@yahoo.es>
2017-05-28 19:45:09 +0200

    Now save to database all articles from RSS (not only one)

--- flask/form.py
+++ flask/form.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request
 import feedparser
 import unicodedata
@@ -17,25 +19,37 @@ def signup():
 	if request.method == 'POST':
 		# First, obtain the introduced text through the form
 		result = request.form
-		date = result['publication-date']
+		required_date = result['publication-date']
 
 		# Get data from the RSS service
 		d = feedparser.parse('http://www.20minutos.es/rss/')
-		text = d.entries[0].summary
 
-		# Encode text to ASCII before analyzing it
-		text = text.encode('ascii', 'ignore')
+		for item in range(len(d)):
+			text = d.entries[item].summary # type: unicode
+			date = d.entries[item].updated[0:10] # type: unicode
 
-		# Analyze it
-		cw = CountWords(text)
-		sorted_list = cw.text_analyzer()
-		print(sorted_list)
+			# Encode text to ASCII before analyzing it
+			#text = text.encode('ascii', 'ignore')
+
+			# Analyze it
+			cw = CountWords(text)
+			sorted_list = cw.text_analyzer()
 		
-		# Save the result to database
-		mongo.db.words.insert({'count': sorted_list})
-
+			# Save the result to database
+			mongo.db.words.insert({'count': sorted_list, 'date': date})
+			
+		result = mongo.db.words.find({'date': '2017-05-28'}) # Aquí es donde habria que decirle que busque por fecha
+		'''
+		for i in range(len(result)):
+			result[i][1] = result[i][1].encode('ascii', 'ignore')
+		'''
+		#print(result[0]['count'][0:6])
+		# Show only top used words
+		list_to_show = []
+		for doc in result:
+			list_to_show.append(doc['count'][0:6])
 		# Pass the text to the template in order to be showed in the screen
-		return render_template('form.html', result = sorted_list);
+		return render_template('form.html', result = list_to_show);
 
 
 if __name__ == "__main__":
